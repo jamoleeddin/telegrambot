5b763fe0853fbf472fd1b2e22cc9e4f5162bfd60
jamoleeddin <133091857+jamoleeddin@users.noreply.github.com>
2023-05-10 12:12:54 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Assalomu alaykum, botga xush kelibsiz!")
+
+def main():
+    updater = Updater(token="TOKEN", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
